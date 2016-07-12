d984235a0d81b463771945e47492eeaceaa9dc1e
ethanow <owch0002@e.ntu.edu.sg>
2016-07-12 23:47:45 +0800

    updated bot to speak

--- index.js
+++ index.js
@@ -30,3 +30,40 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+// Setup API endpoint
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAQiCtAhW0wBAPThSRKufITs12PkEJwRshfPBY69rzZCADHqNrwIJJlinJIBZAcXLBWYPmwUjq9oUo0QptIZBMAD8UkPUndfcYLToD2Wke321SpgQuisnmExwZCFScS8tjWYqOSElaf0a5TNYLnhcqtZB25ZASm7AZCOZAYVAxInHQZDZD"
+
+// Echo messages
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
