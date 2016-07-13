d303d86e3b18fe9e8369959963b71d12b186c557
ethanow <owch0002@e.ntu.edu.sg>
2016-07-14 00:25:33 +0800

    new tokens

--- config.js
+++ config.js
@@ -6,14 +6,12 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAQiCtAhW0wBALjklzYRwUa8n2dyBfcZBCZApZBoI0WZBDTznBMsTJ8eXltZBrNh8PXZA8Q57CL2dDEc1z9qJmMGv5sSO80fQoTpmPCZCNescA8ifhEvaXYA9FZCo66YAPZChFUYWGGpuvFdZAjzHsGefkIs69bxIkCxclYiZAgsAyw8QZDZD'
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAQiCtAhW0wBALjklzYRwUa8n2dyBfcZBCZApZBoI0WZBDTznBMsTJ8eXltZBrNh8PXZA8Q57CL2dDEc1z9qJmMGv5sSO80fQoTpmPCZCNescA8ifhEvaXYA9FZCo66YAPZChFUYWGGpuvFdZAjzHsGefkIs69bxIkCxclYiZAgsAyw8QZDZD';
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
-
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'hello_world'
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
-  FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
 }
+
