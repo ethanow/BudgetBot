ee077689340f0bb567d1722a3acb5222da1e3260
ethanow <owch0002@e.ntu.edu.sg>
2016-07-14 23:57:58 +0800

    reverted to example

--- services/wit.js
+++ services/wit.js
@@ -5,7 +5,7 @@ var FB = require('../connectors/facebook')
 var Wit = require('node-wit').Wit
 var request = require('request')
 
-//Extract an entity value from the entities returned by Wit
+
 var firstEntityValue = function (entities, entity) {
 	var val = entities && entities[entity] &&
 		Array.isArray(entities[entity]) &&
@@ -17,32 +17,6 @@ var firstEntityValue = function (entities, entity) {
 	}
 	return typeof val === 'object' ? val.value : val
 }
-
-
-/*
-//Define your bot functions here
-const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-        console.log('user said...', request.text);
-        console.log('sending...', JSON.stringify(response));
-        return resolve();
-    });
-  },
-  ['compute-result']({context,entities}) {
-    return new Promise(function(resolve, reject) {
-      const movie_title = firstEntityValue(entities, 'movie');
-      if (movie_title) {
-        context.movie = movie_title;
-      }
-      //call the API here
-      return resolve(context);
-  });
- },
-};
-*/
 
 
 var actions = {
@@ -68,7 +42,18 @@ var actions = {
 		
 	},
 
-	logSpend(sessionId, context, entities, message, cb){
+	merge(sessionId, context, entities, message, cb) {
+		// Reset the weather story
+		delete context.forecast
+
+		// Retrive the location entity and store it in the context field
+		var loc = firstEntityValue(entities, 'location')
+		if (loc) {
+			context.loc = loc
+		}
+
+		// Reset the cutepics story
+		delete context.pics
 
 		// Retrieve the category
 		var category = firstEntityValue(entities, 'category')
@@ -76,19 +61,15 @@ var actions = {
 			context.cat = category
 		}
 
-		// Retrieve $ amount
-		var amount = firstEntityValue(entities,'amount_of_money')
-		if (amount) {
-			context.amount = amount
-		}
-
-		var datetime = firstEntityValue(entities,'datetime')
-		if (datetime){
-			context.datetime = datetime
+		// Retrieve the sentiment
+		var sentiment = firstEntityValue(entities, 'sentiment')
+		if (sentiment) {
+			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
+		} else {
+			delete context.ack
 		}
 
 		cb(context)
-
 	},
 
 	error(sessionId, context, error) {
@@ -96,25 +77,29 @@ var actions = {
 	},
 
 	// list of functions Wit.ai can execute
-	['logSpend'](sessionId, context, cb){
+	['fetch-weather'](sessionId, context, cb) {
+		// Here we can place an API call to a weather service
+		// if (context.loc) {
+		// 	getWeather(context.loc)
+		// 		.then(function (forecast) {
+		// 			context.forecast = forecast || 'sunny'
+		// 		})
+		// 		.catch(function (err) {
+		// 			console.log(err)
+		// 		})
+		// }
 
-		context.amount = amount_of_money
-		context.cat = category
-		console.log('Logging noDate')
+		context.forecast = 'Sunny'
+
 		cb(context)
-
 	},
 
-	['logSpendDate'](sessionId, context, cb){
+	['fetch-pics'](sessionId, context, cb) {
+		var wantedPics = allPics[context.cat || 'default']
+		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
 
-		context.amount = amount_of_money
-		context.cat = category
-		context.datetime = datetime
-		console.log('Logging withDate')
 		cb(context)
-
 	},
-	
 }
 
 // SETUP THE WIT.AI SERVICE
@@ -133,3 +118,50 @@ if (require.main === module) {
 	var client = getWit()
 	client.interactive()
 }
+
+// GET WEATHER FROM API
+var getWeather = function (location) {
+	return new Promise(function (resolve, reject) {
+		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+		request(url, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	var jsonData = JSON.parse(body)
+		    	var forecast = jsonData.query.results.channel.item.forecast[0].text
+		      console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
+		      return forecast
+		    }
+			})
+	})
+}
+
+// CHECK IF URL IS AN IMAGE FILE
+var checkURL = function (url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+// LIST OF ALL PICS
+var allPics = {
+  corgis: [
+    'http://i.imgur.com/uYyICl0.jpeg',
+    'http://i.imgur.com/useIJl6.jpeg',
+    'http://i.imgur.com/LD242xr.jpeg',
+    'http://i.imgur.com/Q7vn2vS.jpeg',
+    'http://i.imgur.com/ZTmF9jm.jpeg',
+    'http://i.imgur.com/jJlWH6x.jpeg',
+		'http://i.imgur.com/ZYUakqg.jpeg',
+		'http://i.imgur.com/RxoU9o9.jpeg',
+  ],
+  racoons: [
+    'http://i.imgur.com/zCC3npm.jpeg',
+    'http://i.imgur.com/OvxavBY.jpeg',
+    'http://i.imgur.com/Z6oAGRu.jpeg',
+		'http://i.imgur.com/uAlg8Hl.jpeg',
+		'http://i.imgur.com/q0O0xYm.jpeg',
+		'http://i.imgur.com/BrhxR5a.jpeg',
+		'http://i.imgur.com/05hlAWU.jpeg',
+		'http://i.imgur.com/HAeMnSq.jpeg',
+  ],
+  default: [
+    'http://blog.uprinting.com/wp-content/uploads/2011/09/Cute-Baby-Pictures-29.jpg',
+  ],
+};
